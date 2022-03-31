4a0bf06b2abf3462a91db82b7cba451462846a0b
Rebecca Sissman <rsissman15@aol.com>
2022-03-31 14:50:13 -0400

    Completed Assigment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Question from "./Question";
 import quiz from "../data/quiz";
-
 function App() {
   const [questions, setQuestions] = useState(quiz);
   const [currentQuestionId, setCurrentQuestion] = useState(1);
